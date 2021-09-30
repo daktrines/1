1418fead495fc89ec29ee1a9c2e2028f02ea8531
ИСП-31 <-31@LAB17-01>
2021-09-30 16:18:28 +0300

    Добавьте файлы проекта.

---
+++ 1/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ClassLibrary1;
+
+namespace _1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Info_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Выполнила студентка группы ИСП-31 Калион Екатерина\n" +
+                "Вычислить сумму целых случайных чисел, распределенных в диапазоне от 2 до 10, пока" +
+                "эта сумма не превышает некоторого числа K. Вывести на экран сгенерированные числа, " +
+                "значение суммы, и количество сгенерированных чисел.", "Информация");
+        }
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+        
+        private void Start_Click(object sender, RoutedEventArgs e)
+        {
+            int k = Convert.ToInt32(ZnK.Text); //Вводим к
+            Class1.Start(k, out string str, out int sum, out int kol); //используем функцию
+            Zn.Text = str; //вывод сгенерированных чисел
+            Sum.Text = Convert.ToString(sum); //вывод sum
+            Kol.Text = Convert.ToString(kol); //вывод kol
+        }
+        private void Drzn(object sender, TextChangedEventArgs e)
+        {
+            Zn.Text = null;
+            Sum.Text = null;
+            Kol.Text = null;
+        }
+    }
+}
---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+	public class Class1
+	{
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="k"></param>
+		/// <param name="str"></param>
+		/// <param name="sum"></param>
+		/// <param name="kol"></param>
+		public static void Start(int k, out string str, out int sum, out int kol)
+		{
+			str = ""; // строка с генерирированнными числами
+			sum = 0; //Сумма 
+			kol = 0; //Количество
+			int zn = 0; // число
+			Random rnd = new Random();
+
+			while (sum < k)
+			{
+				//диапазон от 2 до 10 
+				zn = rnd.Next(2, 10);
+				kol++; //увеличиваем счетчик
+				sum = sum + zn; //вычисление суммы
+				str += Convert.ToString(zn) + " , "; //заполнение строки с геннерированными числами
+			}
+		}
+	}
+}
